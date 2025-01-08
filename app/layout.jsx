464e9fe5f799bb1d684453071b6140fdc9227750
SaiGane5 <imandisaiganesh1@gmail.com>
2025-01-08 23:58:17 +0530

    Udemy NextJS Course Traversy

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import "@/assets/styles/globals.css";
+
+const Layout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
